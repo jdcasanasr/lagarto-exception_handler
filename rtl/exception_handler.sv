4a60b8140bdb49488e9471afc28949769d9432c3
Daniel Casañas <jdaniel.casanasr@gmail.com>
2024-07-01 15:44:57 -0600

    ToDo: Implement CSR update loop

--- rtl/exception_handler.sv
+++ rtl/exception_handler.sv
@@ -34,13 +34,9 @@ module exception_handler
     // Machine Trap Setup.
     mstatus_t       mstatus_r;
     misa_t          misa_r;
-    medeleg_t       medeleg_r;
-    mideleg_t       mideleg_r;
     mie_t           mie_r;
     mtvec_t         mtvec_r;
     mcounteren_t    mcounteren_r;
-    mstatush_t      mstatush_r;
-    medelegh_t      medelegh_r;
 
     // Machine Trap Handling.
     mscratch_t      mscratch_r;
@@ -52,25 +48,36 @@ module exception_handler
     mtval2_t        mtval2_r;
 
     // Internal Signals.
-    privilege_level_t   privilege_level_r;
+    privilege_level_t privilege_level_r, privilege_level_w;
 
     // Control Signals.
-    logic               csr_address_exists_w;
-    logic               csr_privilege_violation_w;
+    logic csr_address_exists_w;
+    logic csr_privilege_violation_w;
 
-    logic               csr_read_enable_w;
-    logic               csr_write_enable_w;
+    logic csr_read_enable_w;
+    logic csr_write_enable_w;
 
     // Internal Signals Update.
+    // Note: For the mean time, we'll only support this mode.
+    assign privilege_level_w = MACHINE;
+
+    // Control Signals Update.
     assign csr_address_exists_w         = (csr_address_i inside {CSR_MSTATUS, CSR_MISA, CSR_MEDELEG, CSR_MIDELEG, CSR_MIE, CSR_MTVEC, CSR_MCOUNTEREN, CSR_MSTATUSH, CSR_MEDELEGH, CSR_MSCRATCH, CSR_MEPC, CSR_MCAUSE, CSR_MTVAL, CSR_MIP, CSR_MTINST, CSR_MTVAL2}) ? 1'b1 : 1'b0;
     assign csr_privilege_violation_w    = (csr_address_i.minimum_privilege_level < privilege_level_r) ? 1'b1 : 1'b0;
 
-    // Control Signals Update.
     assign csr_read_enable_w            = (csr_command_i inside {READ_ONLY, WRITE_AND_READ})    && csr_address_exists_w && !csr_privilege_violation_w ? 1'b1 : 1'b0;
     assign csr_write_enable_w           = (csr_command_i inside {WRITE_ONLY, WRITE_AND_READ})   && csr_address_exists_w && !csr_privilege_violation_w ? 1'b1 : 1'b0;
 
     // Output Signals Update.
     assign csr_read_data_valid_o        = csr_address_exists_w && !csr_privilege_violation_w ? 1'b1 : 1'b0;
+
+    // Privilege Level Update.
+    always_ff @ (posedge clock_i, negedge reset_ni)
+        if (!reset_ni)
+            privilege_level_r = MACHINE;
+
+        else
+            privilege_level_r = privilege_level_w;
 
     // CSR Read/Write Loop.
     always_ff @ (posedge clock_i, negedge reset_ni)
@@ -80,13 +87,9 @@ module exception_handler
                 // Machine Trap Setup.
                 mstatus_r       = '0;
                 misa_r          = '0;
-                medeleg_r       = '0;
-                mideleg_r       = '0;
                 mie_r           = '0;
                 mtvec_r         = '0;
                 mcounteren_r    = '0;
-                mstatush_r      = '0;
-                medelegh_r      = '0;
 
                 // Machine Trap Handling.
                 mscratch_r      = '0;
@@ -103,15 +106,11 @@ module exception_handler
                 // M-Mode Registers.
                 // Machine Trap Setup.
                 CSR_MSTATUS:    csr_read_data_o = mstatus_r;
-                CSR_MISA:       csr_read_data_o = misa_r; 
-                CSR_MEDELEG:    csr_read_data_o = medeleg_r;
-                CSR_MIDELEG:    csr_read_data_o = mideleg_r;
+                CSR_MISA:       csr_read_data_o = misa_r;
                 CSR_MIE:        csr_read_data_o = mie_r;
                 CSR_MTVEC:      csr_read_data_o = mtvec_r;
                 CSR_MCOUNTEREN: csr_read_data_o = mcounteren_r;
-                CSR_MSTATUSH:   csr_read_data_o = mstatush_r;
-                CSR_MEDELEGH:   csr_read_data_o = medelegh_r;
-
+                
                 // Machine Trap Handling.
                 CSR_MSCRATCH:   csr_read_data_o = mscratch_r;
                 CSR_MEPC:       csr_read_data_o = mepc_r;
